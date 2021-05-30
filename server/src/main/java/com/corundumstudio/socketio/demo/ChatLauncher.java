bfbe364c95983bc41113adb870a0c3aefe9ed1d7
lixin <13798516431@126.com>
2021-05-30 15:49:18 +0800

    init blog

--- server/src/main/java/com/corundumstudio/socketio/demo/ChatLauncher.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/ChatLauncher.java
@@ -6,12 +6,17 @@ import com.corundumstudio.socketio.*;
 public class ChatLauncher {
 
     public static void main(String[] args) throws InterruptedException {
-
+    	
+    	// 1. 创建配置
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(9092);
+        
 
+        // 2. 创建SocketIOServer(基于Netty)
         final SocketIOServer server = new SocketIOServer(config);
+        
+        // 3. 添加事件处理.
         server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
             @Override
             public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
@@ -20,6 +25,7 @@ public class ChatLauncher {
             }
         });
 
+        // 4. 启动(创建Netty并设置:Pipeline)
         server.start();
 
         Thread.sleep(Integer.MAX_VALUE);
